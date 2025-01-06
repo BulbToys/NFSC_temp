7b71a36567746fe266f574eb320a441136863781
brokenphilip <13336890+brokenphilip@users.noreply.github.com>
2025-01-06 14:29:09 +0100

    fix version string, test drift scores

--- NFSCBulbToys/src/modules/testing.cpp
+++ NFSCBulbToys/src/modules/testing.cpp
@@ -69,6 +69,149 @@ namespace testing
 				}
 			}
 
+			if (ImGui::BulbToys_Menu("Test Drift Scores"))
+			{
+				static bool cs_scoring = false;
+				if (ImGui::Checkbox("Force CS scoring", &cs_scoring))
+				{
+					if (cs_scoring)
+					{
+						Write<uint16_t>(0x6BAE4B, 0x9090);
+					}
+					else
+					{
+						Write<uint16_t>(0x6BAE4B, 0x0A74);
+					}
+				}
+
+				struct PointsVector
+				{
+					int* begin;
+					int* end;
+				};
+
+				auto gracedb = Read<uintptr_t>(0xA9828C);
+				if (gracedb)
+				{
+					auto drift_scoring = Read<uintptr_t>(gracedb + 0x44);
+
+					if (drift_scoring)
+					{
+						int sections = Read<int>(drift_scoring + 0x90);
+						ImGui::Text("Sections: %d", sections);
+
+						if (ImGui::Button("Reset Sections"))
+						{
+							auto section_map = drift_scoring + 0x7C;
+
+							/*
+								DoNukeSubtree(&this->mSectionMap.mAnchor.mpNodeParent->mpNodeRight);
+								this->mSectionMap.mAnchor.mpNodeLeft = &this->mSectionMap.mAnchor;
+								this->mSectionMap.mAnchor.mpNodeParent = 0;
+								this->mSectionMap.mAnchor.mColor = 0;
+								this->mSectionMap.mnSize = 0;
+								this->mSectionMap.mAnchor.mpNodeRight = &this->mSectionMap.mAnchor;
+							*/
+
+							reinterpret_cast<void(__stdcall*)(uintptr_t)>(0x6ACE50)(Read<uintptr_t>(section_map + 0xC));
+							Write<uintptr_t>(section_map + 0x8, section_map + 0x4);
+							Write<uintptr_t>(section_map + 0xC, 0);
+							Write<uint8_t>(section_map + 0x10, 0);
+							Write<uint32_t>(section_map + 0x14, 0);
+							Write<uintptr_t>(section_map + 0x4, section_map + 0x4);
+						}
+
+						PointsVector pts[3];
+
+						for (int i = 0; i < 3; i++)
+						{
+							int racer_index = i + 2;
+							pts[i] = *reinterpret_cast<PointsVector * (__thiscall*)(uintptr_t, int*)>(0x6BA960)(drift_scoring + 0x98, &racer_index);
+						}
+
+						if (ImGui::BeginTable("ScoresTable", 4, ImGuiTableFlags_SizingFixedFit))
+						{
+							ImGui::TableNextRow();
+
+							ImGui::TableSetColumnIndex(0);
+							ImGui::Text("Seg");
+
+							ImGui::TableSetColumnIndex(1);
+							ImGui::Text("Racer 1");
+
+							ImGui::TableSetColumnIndex(2);
+							ImGui::Text("Racer 2");
+
+							ImGui::TableSetColumnIndex(3);
+							ImGui::Text("Racer 3");
+
+							bool more[3] = { true };
+							for (int row = 0; more[0] || more[1] || more[2]; row++)
+							{
+								ImGui::TableNextRow();
+
+								for (int column = 0; column < 4; column++)
+								{
+									ImGui::TableSetColumnIndex(column);
+
+									if (column == 0)
+									{
+										ImGui::Text("%d", row + 1);
+									}
+
+									if (column == 1)
+									{
+										if (pts[0].begin == pts[0].end)
+										{
+											ImGui::Text("X");
+											more[0] = false;
+										}
+										else
+										{
+											auto points = *pts[0].begin;
+											ImGui::Text("%d", points);
+											pts[0].begin++;
+										}
+									}
+
+									if (column == 2)
+									{
+										if (pts[1].begin == pts[1].end)
+										{
+											ImGui::Text("X");
+											more[1] = false;
+										}
+										else
+										{
+											auto points = *pts[1].begin;
+											ImGui::Text("%d", points);
+											pts[1].begin++;
+										}
+									}
+
+									if (column == 3)
+									{
+										if (pts[2].begin == pts[2].end)
+										{
+											ImGui::Text("X");
+											more[2] = false;
+										}
+										else
+										{
+											auto points = *pts[2].begin;
+											ImGui::Text("%d", points);
+											pts[2].begin++;
+										}
+									}
+								}
+							}
+
+							ImGui::EndTable();
+						}
+					}
+				}
+			}
+
 			if (ImGui::BulbToys_Menu("Test Packages"))
 			{
 				static char* packages[115]{ nullptr };
@@ -114,7 +257,6 @@ namespace testing
 
 			if (ImGui::BulbToys_Menu("Testing"))
 			{
-				// Replace Statistics with Template
 				static bool replace = false;
 				if (ImGui::Checkbox("Replace Statistics with Template", &replace))
 				{
@@ -134,7 +276,6 @@ namespace testing
 
 				ImGui::Separator();
 
-				// Current error
 				uintptr_t error_ptr = Read<uintptr_t>(0xA97B70) + 0x18;
 				static int error = 0;
 				if (error_ptr != 0x18)
@@ -143,7 +284,6 @@ namespace testing
 				}
 				ImGui::Text("Current error: %d", error);
 
-				// Error1 & Error2
 				if (ImGui::Button("Error1"))
 				{
 					Write<int>(error_ptr, 1);
@@ -154,7 +294,6 @@ namespace testing
 					Write<int>(error_ptr, 2);
 				}
 
-				// AdvancedError
 				if (ImGui::Button("AdvancedError"))
 				{
 					// todo
@@ -162,7 +301,6 @@ namespace testing
 
 				ImGui::Separator();
 
-				// Chyron Message
 				static char msg[128];
 				ImGui::InputText("##CHYmsg", msg, IM_ARRAYSIZE(msg));
 
@@ -185,14 +323,11 @@ namespace testing
 					}
 				}
 
-				// Icon
 				ImGui::Text("Icon: %d", icon);
 
-				// Linger
 				static bool linger = false;
 				ImGui::Checkbox("Linger", &linger);
 
-				// Chyron Test
 				if (ImGui::Button("Chyron Test"))
 				{
 					NFSC::FE_ShowChyron(msg, icon, linger);
